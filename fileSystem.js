3ed0e2996cb421ddd3704d96062fd584d2e16be8
Animesh Karmakar <animesh.pust@gmail.com>
2019-07-21 17:39:16 +0600

    file renamed

--- fileSystem.js
+++ fileSystem.js
@@ -1,16 +1,33 @@
+// Cearting, Reading, Writing, Appending in file 
 fs = require('fs');
 
 fs.writeFile('example.txt', 'This is an example file', (err)=>{
 	if(err)
-		console.log('Error occured to write file');
+		console.log(err);
 	else{
 		console.log('File created successfully');
 
 		fs.readFile('example.txt','UTF-8', (err, file)=>{
 			if(err)
-				console.log('Error occured to read file');
+				console.log(err);
 			else 
 				console.log('file output : ' + file);
 		})
 	}
 });
+
+// fs.rename('example.txt', 'Animesh.txt', (err)=>{
+// 	if(err)
+// 		console.log(err);
+// 	else 
+// 		console.log('file renamed successfully');
+// });
+
+
+fs.appendFile('Animesh.txt', '\nsome data being appended', (err)=>{
+	if(err)
+		console.log(err);
+	else 
+		console.log('\n Data appended successfully\n');
+
+})
