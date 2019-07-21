d04cbb7bbe6ec0d3c815883eefb893fd705f53d1
Animesh Karmakar <animesh.pust@gmail.com>
2019-07-21 22:17:55 +0600

    a static server which transact a html file

---
+++ static/app.js
@@ -0,0 +1,11 @@
+const http = require('http'); // http module
+const fs = require('fs');    // file system module
+
+http.createServer((req, res)=> {
+	const readStream = fs.createReadStream('./Animesh.html', 'utf8');
+	res.writeHead(200, {'content-type':'text/html'}); // telling server what kind of file client may send
+	readStream.pipe(res);  // making a pipeline with server and clinet and can make transaction
+
+}).listen(3000);  //port to listen server 
+
+console.log('Server has started');
